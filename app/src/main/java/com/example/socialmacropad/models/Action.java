6e19b36e76d12e70f9559dae7cdcd9e8e38ec3ca
memoriasIT <memoriasdeuninformatico2017@gmail.com>
2021-01-06 17:32:54 +0100

    modify model for action

--- app/src/main/java/com/example/socialmacropad/models/Action.java
+++ app/src/main/java/com/example/socialmacropad/models/Action.java
@@ -3,14 +3,24 @@ package com.example.socialmacropad.models;
 public class Action {
     private String Action;
     private String ActionName;
+    private String color;
 
     // ESTO ESTÄ BIEN ES PARA FIREBASE
     public Action(){}
 
-    public Action(String Action, String ActionName){
+    public Action(String Action, String ActionName, String color){
         super();
         this.Action = Action;
         this.ActionName = ActionName;
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
     }
 
     public String getAction() {
